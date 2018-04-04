6e2485765302919fd74d2b18f6726b1bd86e453f
Kavishree-Shanmugam <kavishree.ece@gmail.com>
2018-04-04 09:49:32 +0530

    My Python Codings

--- Hangman Game/hangman_game.py
+++ Hangman Game/hangman_game.py
@@ -1,3 +1,5 @@
+import random
+
 class Hangman:
     def __init__(self):
         print "**** WELCOME TO HANGMAN GAME.... **** \n\t(1)Yes --> to START the game... \n\t(2)No -> to EXIT from the game\n"
@@ -13,15 +15,26 @@ class Hangman:
 
     def start_game(self):
         guess_count = 6
-        secret_word = "secret"
+        list_words = ["secret", "Hangman", "congratulations"]
+        secret_word = random.choice(list_words)
+        letters_used = []
 
         while guess_count > 0:
             guess_letter = raw_input("Enter you guessing letter : ")
-
-            if guess_letter in secret_word:
+            if not guess_letter:
+                print "Please Enter some Letter...!!!"
+                continue
+            secret_word = secret_word.lower()
+            guess_letter = guess_letter.lower()
+            if guess_letter in secret_word :
                 print "Wow...Guessed correct letter"
-                guess_count -= 1
                 secret_word = secret_word.replace(guess_letter, '', 1)
+                letters_used.append(guess_letter)
+                continue
+            if guess_letter not in secret_word and guess_letter in letters_used:
+                print "Already this letter got filled.."
+                print "Try with some other letter...!!!"
+                continue
             else:
                 print "Sorry...Wrong guess..Try again..."
                 self.hangman_hang(guess_count)
